c7d4bb1772f0db0d3f1c192f5f794af62327b01a
Christian Corrales <chriscorrales29@gmail.com>
2022-09-30 12:16:40 -0300

    Configura eslint e prettier e formata os arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-  )
+  return <button onClick={() => setCount(count => count + 1)}>count is {count}</button>;
 }
 
-export default App
+export default App;
